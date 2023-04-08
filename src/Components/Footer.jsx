a04d73c75de4c31f3248026eb6d146b5b0886562
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-08 09:52:22 -0500

    modifiqué el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: '#333', color: '#fff', width: '100%', padding: '20px 0', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <footer className='footer' style={{ backgroundColor: '#333', color: '#fff', width: '100%', padding: '20px 0', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <p>Powered by</p>
         <img src="img/DH.png" alt='DH-logo' />
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <a href="https://www.facebook.com" target="_blank"><img src="img/ico-facebook.png" alt='Facebook-logo' className="footer-icon" /></a>
-            <a href="https://www.instagram.com" target="_blank"><img src="img/ico-instagram.png" alt='Instagram-logo' className="footer-icon" /></a>
-            <a href="https://www.tiktok.com" target="_blank"><img src="img/ico-tiktok.png" alt='Tiktok-logo' className="footer-icon" /></a>
-            <a href="https://www.whatsapp.com" target="_blank"><img src="img/ico-whatsapp.png" alt='Whatsapp-logo' className="footer-icon" /></a>
+            <img src="img/ico-facebook.png" alt='Facebook-logo' style={{ width: '2vw', height: '2vw', margin: '0 1em' }} />
+            <img src="img/ico-instagram.png" alt='Instagram-logo' style={{ width: '2vw', height: '2vw', margin: '0 1em' }} />
+            <img src="img/ico-tiktok.png" alt='Tiktok-logo' style={{ width: '2vw', height: '2vw', margin: '0 1em' }} />
+            <img src="img/ico-whatsapp.png" alt='Whatsapp-logo' style={{ width: '2vw', height: '2vw', margin: '0 1em' }} />
         </div>
     </footer>
   )
